55f2843e4b3549f47887d73d7560e98e1f2b0437
Li Fei <hefrankeleyn@gamil.com>
2023-10-02 16:40:00 +0800

    图片

---
+++ projects/next-live-card/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="page">
+		<view class="card"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css");
+</style>
