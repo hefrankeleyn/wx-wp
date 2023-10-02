be5bb2a7657cdb6de9115cadb551c5d01d17c151
Li Fei <hefrankeleyn@gamil.com>
2023-10-02 17:43:49 +0800

    微信小程序

--- projects/next-live-card/pages/index/index.vue
+++ projects/next-live-card/pages/index/index.vue
@@ -1,6 +1,24 @@
 <template>
 	<view class="page">
-		<view class="card"></view>
+		<view class="card">
+			<view class="logo_wapper">
+				<image src="../../static/001logo.png" mode="widthFix" class="logo"></image>
+			</view>
+			<view class="info">
+				<view class="name">
+					李飞
+				</view>
+				<view class="desc">
+					认真且持之以恒的学习和实践
+				</view>
+				<view>
+					Lf_Ren
+				</view>
+				<view>
+					hefrankeleyn@gmail.com
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
